168dbc05473a2a368a702e90697ac66fecfe859e
anjalisingh2 <93580723+anjalisingh2@users.noreply.github.com>
2026-03-15 09:11:01 +0530

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ api_key = st.sidebar.text_input("Enter Gemini API Key", type="password")
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-1.5-flash')
 
     # User Input
     jd_text = st.text_area("Paste Job Description (JD) here:")
